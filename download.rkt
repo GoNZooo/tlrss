54f0a29af15185636e36390b74ec5ac64c882edc
Rickard Andersson <rickard.m.andersson@gmail.com>
2014-03-27 12:01:09 +0200

    Added some comments and reduced scope as was possible for function definitions.

--- download.rkt
+++ download.rkt
@@ -7,50 +7,68 @@
          "xml-parse.rkt"
          "configuration.rkt")
 
-(provide url->file)
-
-(define (url->file url [base-path ""] [filename ""])
-  (define (get-filename)
-     (if (equal? filename "")
-         (last (string-split url "/"))
-         filename))
-  
-  ; Open the input-port from the URL, copy
-  ; everything to the output port for the file
-  (copy-port (get-pure-port (string->url url))
-             (open-output-file (string-append base-path
-                                              (get-filename))
-                               #:exists 'replace)))
-
-(define (get-rss-data)
-  (define (generate-xexpr)
-    (define (url->port)
-      (get-pure-port (string->url user-rss-url)))
-    
-    (xml->xexpr
-     ((eliminate-whitespace '(item))
-      (document-element (read-xml (url->port))))))
-
-  (generate-xexpr))
-
-(define (match-rss-item i downloaded)
-  (define (downloaded?)
-    (not (false? (member (rss-item-link i) downloaded))))
-  (define (match-downloads set)
-    (cond
-     [(null? set) #f]
-     [(downloaded?)
-      #f]
-     [(regexp-match (cdr (first set))
-                    (rss-item-title i))
-      #t]
-     [else
-      (match-downloads (rest set))]))
-
-  (match-downloads downloads))
-
 (define (fetch-match-loop [downloaded '()])
+  ;; The main function that fetches RSS items
+  ;; and loops through them to find matches
+  
+  (define (url->file url [base-path ""] [filename ""])
+    (define (get-filename)
+      (if (equal? filename "")
+          (last (string-split url "/"))
+          filename))
+    
+    ; Open the input-port from the URL, copy
+    ; everything to the output port for the file
+    (copy-port (get-pure-port (string->url url))
+               (open-output-file (string-append base-path
+                                                (get-filename))
+                                 #:exists 'replace)))
+  
+  (define (get-rss-data)
+    ;; Function that fetches RSS and generates
+    ;; an x-expression from it.
+    
+    (define (generate-xexpr)
+      (define (url->port)
+        (get-pure-port (string->url user-rss-url)))
+      
+      (xml->xexpr
+       ((eliminate-whitespace '(item))
+        (document-element (read-xml (url->port))))))
+
+    (generate-xexpr))
+  
+  (define (match-rss-item? i)
+    ;; Function that matches an item against
+    ;; user-specified downloads to look for.
+    
+    (define (match-downloads set)
+      (define (downloaded?)
+        (not (false? (member (rss-item-link i) downloaded))))
+      
+      (cond
+       ;; If the set is empty, we return false.
+       [(null? set) #f]
+       ;; If the link is already in downloaded links
+       ;; we return false.
+       [(downloaded?)
+        #f]
+       ;; If the regexp we have in 'downloads' matches
+       ;; the name of the current rss-item we have a match.
+       [(regexp-match (cdr (first set))
+                      (rss-item-title i))
+        #t]
+       ;; Otherwise we move on in the download-set.
+       [else
+        (match-downloads (rest set))]))
+
+    ;; Note that 'downloads' refers to the
+    ;; user-defined variable in 'configuration.rkt'.
+    (match-downloads downloads))
+  
   (define (compose-current-time)
+    ;; Formats current time for log/output.
+    
     (define (leading-zero n)
       (if (< n 10)
           (string-append "0" (number->string n))
@@ -74,7 +92,7 @@
         (for-each
          (lambda (rss-entry)
            (begin
-             (when (match-rss-item rss-entry downloaded)
+             (when (match-rss-item? rss-entry)
                (begin
                  (printf "~a - Matched: ~a~n"
                          (compose-current-time)
@@ -87,7 +105,12 @@
          (get-items (get-rss-data)))
         (printf "~a - Fetched rss.~n"
                 (compose-current-time))))
+    (collect-garbage)
     (sleep 310)
+
+    ;; TODO: Log time of addition to downloaded
+    ;; At each iteration, loop through and remove
+    ;; the ones that are too old.
     (fetch-match-loop downloaded)))
 
 (module+ main
