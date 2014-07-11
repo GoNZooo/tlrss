46becba544e0631fd81e57c7229664ad8ac8b207
Rickard Andersson <rickard.m.andersson@gmail.com>
2014-07-12 01:11:10 +0300

    Removed apply-chain macro and simplified structure of config-lang process

--- macros/config-lang.rkt
+++ macros/config-lang.rkt
@@ -18,15 +18,8 @@
   (with-syntax ([def-symbol (format-id stx "~a" sym)]
                 [rvalue (syntax->datum value)])
     #'(begin
-        (define (def-symbol)
-          rvalue)
+        (define def-symbol rvalue)
         (provide def-symbol))))
-
-(define-for-syntax (apply-chain data funcs)
-    (if (null? funcs)
-        data
-        (apply-chain ((car funcs) data)
-                     (cdr funcs))))
 
 (define-syntax (base-path stx)
 
@@ -37,50 +30,45 @@
                  (path? (syntax->datum #'p)))
              "Expected path-string or built path"
              #:fail-unless
-             (apply-chain #'p
-                          `(,syntax->datum
-                            ,build-path
-                            ,expand-user-path
-                            ,directory-exists?))
+             (directory-exists?
+              (expand-user-path
+               (build-path
+                (syntax->datum #'p))))
              "Directory must exist"))
   
-  (syntax-parse
-   stx
+  (syntax-parse stx
    [(_ spath:pathstr)
-    (with-syntax ([built-path (apply-chain #'spath
-                                           `(,syntax->datum
-                                             ,build-path
-                                             ,expand-user-path))])
+    (with-syntax ([built-path
+                   (expand-user-path
+                    (build-path
+                     (syntax->datum #'spath)))])
       (def-provide-symbol stx 'user-base-path #'built-path))]))
 
 (define-syntax (rss-url stx)  
-  (syntax-parse
-   stx
-   [(_ surl:str)
-    (def-provide-symbol stx 'user-rss-url #'surl)]))
+  (syntax-parse stx
+    [(_ surl:str)
+     (def-provide-symbol stx 'user-rss-url #'surl)]))
 
 (define-syntax (downloads stx)
   (define-syntax-class rxstr
     (pattern s #:fail-unless (string? (syntax->datum #'s)) "String needed"))
   
-  (syntax-parse
-   stx
-   [(_ sregex:rxstr ...)
-    (with-syntax ([def-symbol (format-id stx "~a" 'user-downloads)]
-                  [compiled (map regexp (syntax->datum #'(sregex ...)))])
-      #'(begin
-          (define (def-symbol)
-            (quote compiled))
-          (provide def-symbol)))]))
+  (syntax-parse stx
+    [(_ sregex:rxstr ...)
+     (with-syntax ([def-symbol (format-id stx "~a" 'user-downloads)]
+                   [compiled (map pregexp (syntax->datum #'(sregex ...)))])
+       #'(begin
+           (define def-symbol (quote compiled))
+           (provide def-symbol)))]))
 
 (module+ main
-  (base-path "~/torrents/torrentfiles")
+  (base-path "~")
   (rss-url "HejHej")
   (downloads
    "The.Big.Bang.Theory.S07E\\d\\d.720p.*"
    "Hells.Kitchen.US.S12E\\d\\d.*x264.*"
    "Game.of.Thrones.S\\d\\dE\\d\\d.720p.*")
-
-  (user-base-path)
-  (user-rss-url)
-  (user-downloads))
+  (printf "User base path: ~a~nRSS URL: ~a~nDownloads: ~a~n"
+          user-base-path
+          user-rss-url
+          user-downloads))
